18a5f77abe168d5012566d94f9d5cb6f38fe958b
PAVIA Pierre <pierre.pavia@assia.fr>
2023-09-12 21:12:53 +0200

    init

---
+++ springkeycloak-client/src/main/java/ppa/lab/springkeycloakclient/SpringkeycloakClientApplication.java
@@ -0,0 +1,13 @@
+package ppa.lab.springkeycloakclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringkeycloakClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringkeycloakClientApplication.class, args);
+	}
+
+}
---
+++ springkeycloak-client/src/test/java/ppa/lab/springkeycloakclient/SpringkeycloakClientApplicationTests.java
@@ -0,0 +1,13 @@
+package ppa.lab.springkeycloakclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringkeycloakClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
